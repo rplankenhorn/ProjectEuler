78e40aa7592263aaf821e10fddc7872707c3e07c
Robbie Plankenhorn <rplankenhorn@gmail.com>
2016-05-18 11:26:50 -0500

    Completed Problem 36

---
+++ Problem36/Problem36/main.swift
@@ -0,0 +1,33 @@
+//
+//  main.swift
+//  Problem36
+//
+//  Created by Robbie Plankenhorn on 5/18/16.
+//  Copyright © 2016 Plankenhorn. All rights reserved.
+//
+
+import Foundation
+
+extension UInt {
+    
+    func isPalindron() -> Bool {
+        let decimalString = "\(self)"
+        let binaryString = String(self, radix: 2)
+        return binaryString == String(binaryString.characters.reverse()) && decimalString == String(decimalString.characters.reverse())
+    }
+    
+}
+
+var sum:UInt = 0
+
+for i in 0..<1000000 {
+    if i % 10000 == 0 {
+        print(i)
+    }
+    
+    if UInt(i).isPalindron() {
+        sum += UInt(i)
+    }
+}
+
+print(sum)
